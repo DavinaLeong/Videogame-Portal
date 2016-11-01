e7305a23240c6db75be85ea794df24e0327cb3c7
Davina Leong <davina_leong@theshipyard.sg>
2016-11-01 10:00:55 +0800

    - sql script corrected.

--- sql_script.sql
+++ sql_script.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 DROP TABLE IF EXISTS `user_log`;
 CREATE TABLE IF NOT EXISTS `user_log` (
 	`ulid` INT(11) unsigned NOT NULL AUTO_INCREMENT,
-	`user_id` INT(11) NOT NULL,
+	`user_id` INT(11) unsigned NOT NULL,
 	`message` TEXT NOT NULL,
 	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`ulid`)
@@ -30,11 +30,11 @@ CREATE TABLE IF NOT EXISTS `user_log` (
 
 DROP TABLE IF EXISTS `videogames`;
 CREATE TABLE IF NOT EXISTS `videogames` (
-	`vg_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`vg_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
 	`vg_name` VARCHAR(64) NULL,
 	`vg_abbr` VARCHAR(64) NOT NULL,
-	`genre_id` INT(11) NULL,
-	`platform_id` INT(11) NULL,
+	`genre_id` INT(11) unsigned NULL,
+	`platform_id` INT(11) unsigned NULL,
 	`date_purchased` DATE NULL,
 	`from_steam` INT(11) NOT NULL,
 	`date_added` DATETIME NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `videogames` (
 
 DROP TABLE IF EXISTS `game_genre`;
 CREATE TABLE IF NOT EXISTS `game_genre` (
-	`genre_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`genre_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
 	`genre_name` VARCHAR(32) NOT NULL,
 	`genre_abbr` VARCHAR(32) NULL,
 	`genre_description` VARCHAR(512) NULL,
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `game_genre` (
 
 DROP TABLE IF EXISTS `game_platform`;
 CREATE TABLE IF NOT EXISTS `game_platform` (
-	`platform_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`platform_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
 	`platform_name` VARCHAR(64) NOT NULL,
 	`year_intro` INT(11) NOT NULL,
 	`platform_developer` VARCHAR(128) NOT NULL,
@@ -73,18 +73,18 @@ CREATE TABLE IF NOT EXISTS `game_platform` (
 
 DROP TABLE IF EXISTS `screenshot`;
 CREATE TABLE IF NOT EXISTS `screenshot` (
-	`ss_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`ss_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
 	`ss_name` VARCHAR(512) NOT NULL DEFAULT "Screenshot Name",
 	`ss_url` VARCHAR(512) NOT NULL DEFAULT 'screenshots/default_screenshot.png',
 	`ss_thumb_url` VARCHAR(512) NOT NULL DEFAULT 'screenshots/thumbnails/default_thumbnail.png',
 	`ss_description` VARCHAR(512) NOT NULL,
 	`ss_type_id` INT(11) NOT NULL,
-	`vg_id` INT(11) NOT NULL,
-	`ss_width` INT(11) UNSIGNED NULL DEFAULT '854',
-	`ss_height` INT(11) UNSIGNED NULL DEFAULT '480',
+	`vg_id` INT(11) unsigned NOT NULL,
+	`ss_width` INT(11) unsigned NULL DEFAULT '854',
+	`ss_height` INT(11) unsigned NULL DEFAULT '480',
 	`ss_img_type` VARCHAR(64) NULL DEFAULT 'image/png',
-	`ss_thumb_width` INT(11) UNSIGNED NULL DEFAULT '854',
-	`ss_thumb_height` INT(11) UNSIGNED NULL DEFAULT '480',
+	`ss_thumb_width` INT(11) unsigned NULL DEFAULT '854',
+	`ss_thumb_height` INT(11) unsigned NULL DEFAULT '480',
 	`ss_thumb_img_type` VARCHAR(64) NULL DEFAULT 'image/png',
 	`date_added` DATETIME NOT NULL,
 	`last_updated` TIMESTAMP NOT NULL,
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `screenshot` (
 
 DROP TABLE IF EXISTS `screenshot_type`;
 CREATE TABLE IF NOT EXISTS `screenshot_type` (
-	`ss_type_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`ss_type_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
 	`ss_type_name` VARCHAR(20) NOT NULL,
 	`ss_type_description` VARCHAR(128) NOT NULL,
 	`date_added` DATETIME NOT NULL,
